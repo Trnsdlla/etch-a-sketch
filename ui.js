ad29ea956d57e93fce6d58ed5cbdd351901ae5b1
Michael Trandella <107638701+Trnsdlla@users.noreply.github.com>
2024-04-30 13:15:28 -0400

    Add prompt for user input of grid item dimensions

--- ui.js
+++ ui.js
@@ -1,21 +1,37 @@
+let gridSize = parseInt(prompt("Enter # of squares per side for your new grid.", 16))
 
-const gridContainer = document.getElementById("grid-container");
-
-function createGrid() {
-    for (let i = 1; i <= 256; i++) {
-        const div = document.createElement("div");
-        div.classList.add("grid-item");
-        gridContainer.appendChild(div);
+function gridGeneration() {
+    if (gridSize >= 1 && gridSize <= 100) {
+        for (let i = 1; i <= (gridSize * gridSize); i++) {
+            const div = document.createElement("div");
+            div.classList.add("grid-item");
+            div.style.width = gridSize + "px";
+            div.style.height = gridSize + "px";
+            gridContainer.appendChild(div);
+        };
     };
 };
 
-createGrid();
+
+const gridContainer = document.getElementById("grid-container");
 
 gridContainer.addEventListener("mousemove", function(event) {
     if (event.target.classList.contains("grid-item")) {
         event.target.style.backgroundColor = "black";
     }
 });
+
+gridGeneration();
+
+// function createGrid() {
+//     for (let i = 1; i <= 256; i++) {
+//         const div = document.createElement("div");
+//         div.classList.add("grid-item");
+//         gridContainer.appendChild(div);
+//     };
+// };
+
+// createGrid();
 
 // ** Concise but not specific, overwrites existing styles **
 // 
